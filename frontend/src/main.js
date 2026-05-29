720d594f607964d3fa76dbfeb7411b112ba15dee
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2026-05-29 12:19:45 +0200

    minor update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,4 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
-// TODO TypeError: routerHistory.createHref is not a function
-
-
 createApp(App).use(router).mount('#app')
